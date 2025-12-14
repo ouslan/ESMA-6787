071c6d5994f92be59bce82ac8af9bbd2ae7d99d4
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-12-13 21:32:26 -0400

    added code block for problem 2b

---
+++ homeworks/exam-02/code/problem2b.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def main() -> None:
+    df = pd.read_csv("marketing.txt", sep=r"\s+")
+    plt.figure(figsize=(8, 6))
+
+    sns.lineplot(
+        data=df, x="Fee", y="WorkIndex", hue="Supervisory", marker="o", palette="tab10"
+    )
+    plt.title("Interaction Plot of Fee Schedule and Supervisory on WorkIndex")
+    plt.xlabel("Fee Schedule")
+    plt.ylabel("WorkIndex (Quality of Work)")
+    plt.legend(title="Supervisory", loc="upper left")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
